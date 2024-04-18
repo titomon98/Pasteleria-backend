9d5f3d6fedd7f3ef5dae1f64bceead1bdec4c885
titomon98 <arturomonterroso3@gmail.com>
2024-04-18 16:37:03 -0600

    Agregando codigo

---
+++ src/main/java/com/pasteleria/springboot/PasteleriaJirehApplication.java
@@ -0,0 +1,13 @@
+package com.pasteleria.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteleriaJirehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteleriaJirehApplication.class, args);
+	}
+
+}
