5ada3ad67ec5796847bef20c6054be0f73518580
titomon98 <arturomonterroso3@gmail.com>
2024-04-18 16:39:58 -0600

    Ya todo bien

---
+++ src/main/java/com/pasteleria/service/PasteleriaJirehApplication.java
@@ -0,0 +1,13 @@
+package com.pasteleria.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasteleriaJirehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasteleriaJirehApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pasteleria/service/PasteleriaJirehApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasteleria.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteleriaJirehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
